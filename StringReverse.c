37fc1502bd4d4a323242abf835e7cdf62d185fea
David <48716362+oldskool978@users.noreply.github.com>
2020-09-15 01:04:47 -0400

    Create StringReverse.c

---
+++ StringReverse.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+char *GetString();
+void ReverseString(char *StringPTR);
+void FreeString(char * StringPTR);
+
+main(){
+    char* String = GetString();
+    ReverseString(String); 
+    FreeString(String);
+}
+
+char *GetString()
+{
+    char *FullString = calloc(0,sizeof(char));
+    int LastChar;
+    for (int i = 0, CurrentChar; (CurrentChar = getchar()) != EOF;i++)
+    {
+        FullString = realloc(FullString,sizeof(char)* i + 1);
+        FullString[i] = CurrentChar;
+        LastChar = i + 1;
+    }
+    FullString = realloc(FullString,sizeof(char)* LastChar + 1);
+    FullString[LastChar]= '\0';
+    return FullString;
+}
+
+void FreeString(char * StringPTR)
+{
+    free(StringPTR);
+}
+
+void ReverseString(char *StringPTR)
+{
+    
+    int OffsetTracker = 0, LineTracker = 0, CharCounter = 0;
+    while (StringPTR[LineTracker] != '\0')
+    {
+        CharCounter++;
+        if (StringPTR[LineTracker] == '\n')
+        {
+            for (int i = LineTracker; i > LineTracker - CharCounter; i--)
+                printf("%c",StringPTR[i]);
+            CharCounter = 0;
+        }
+        LineTracker++;
+    }
+}
